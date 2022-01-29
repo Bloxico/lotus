b27a16a4f9ea5ae685c3234d7a76393a1dd34b66
TheMenko <themenkoprojects@gmail.com>
2022-01-29 18:21:20 +0100

    block by ips

--- itests/conngater_test.go
+++ itests/conngater_test.go
@@ -2,11 +2,13 @@ package itests
 
 import (
 	"context"
+	"reflect"
 	"testing"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/itests/kit"
 	"github.com/libp2p/go-libp2p-core/peer"
+	manet "github.com/multiformats/go-multiaddr/net"
 	"github.com/stretchr/testify/require"
 )
 
@@ -47,6 +49,54 @@ func TestNetBlockPeer(t *testing.T) {
 
 	if len(list.Peers) > 0 {
 		t.Errorf("failed to remove blocked peer from blocked peer list")
+	}
+
+}
+
+func TestNetBlockIPAddr(t *testing.T) {
+	ctx := context.Background()
+
+	firstNode, secondNode, _, ens := kit.EnsembleTwoOne(t)
+	ens.InterconnectAll()
+
+	addrInfo, _ := secondNode.NetAddrsListen(ctx)
+
+	var secondNodeIPs []string
+
+	for _, addr := range addrInfo.Addrs {
+		ip, err := manet.ToIP(addr)
+		if err != nil {
+			continue
+		}
+		secondNodeIPs = append(secondNodeIPs, ip.String())
+	}
+
+	err := firstNode.NetBlockAdd(ctx, api.NetBlockList{IPAddrs: secondNodeIPs})
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	list, err := firstNode.NetBlockList(ctx)
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	if len(list.IPAddrs) == 0 || reflect.DeepEqual(list.IPAddrs, secondNodeIPs) {
+		t.Errorf("blocked ip not in blocked ip list")
+	}
+
+	err = firstNode.NetBlockRemove(ctx, api.NetBlockList{IPAddrs: secondNodeIPs})
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	list, err = firstNode.NetBlockList(ctx)
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	if len(list.IPAddrs) > 0 {
+		t.Errorf("failed to remove blocked ip from blocked ip list")
 	}
 
 }
